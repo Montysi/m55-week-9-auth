8a7e86323d30099821a5d1e33810fe4d5c4916be
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-07-17 11:19:31 +0100

    feature: add comparePass function

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -21,31 +21,29 @@ const hashPass = async (req, res, next) => {
 const comparePass = async (req, res, next) => {
   try {
         
-        
-
-
-
-        // const comparedPassword = await bcrypt.compare(req.body.password, user.get("password")) 
-            
         const user = await User.findOne({ where: { username: req.body.username } });
 
-        req.user = user;
+        if (!user) {
+            return res.status(404).json({ message: "Unable to find user." });
+        };
 
-        next();
+        const comparedPassword = await bcrypt.compare(req.body.password, user.password); 
+
+        if (comparedPassword) {
+             req.user = user;
+            next();
+        } else {
+            res.status(401).json({ message: "Passwords do not match"});
+        };
+
+       
   } catch (error) {
     res.status(500).json({ message: error.message, error });
   }
 };
 
 
- // bcrypt.compare takes 2 parameters, the plaintext password and the hashed password from the db
-    // get user from db with the username
-    // check if user exists
-    // compare password
-    // check if return value is true or false
-    // if false response "passwords do not match"
-    // attach user to the request
-    // next
+ 
 
 
 
